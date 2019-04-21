c4a93461b07c234face4d9c470212e4e04349466
keason10 <729662970@qq.com>
2019-04-21 17:55:21 +0800

    添加了swagger

--- springboot/src/main/java/com/wykj/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/wykj/springboot/SpringbootApplication.java
@@ -3,9 +3,17 @@ package com.wykj.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+/*
+1. maven添加jar
+2. @EnableSwagger2
+3  访问 http://localhost:port/swagger-ui.html
+4. 文档生成用swagger注解
+ * */
+@EnableSwagger2
 public class SpringbootApplication {
 
     public static void main(String[] args) {
