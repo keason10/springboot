72a7e99d5e1956af16f192e46d1819bf7be3bc53
keasonyan <keasonyan@tencent.com>
2022-04-27 10:48:07 +0800

    添加单元测试用例

--- springboot/src/main/resources/shell/shell-fun.sh
+++ springboot/src/main/resources/shell/shell-fun.sh
@@ -3,3 +3,20 @@
 function prepend(){
   [ -d "$2" ] && eval $1=\"$2\${$1:+':'\$$1\}\" && export $1;}
 }
+
+:'
+1--- 和 2 -- 表明:+不改变原值，当tx不为空，就执行后边的 'hello'$tx 语句
+1---
+tx='222'
+echo ${tx:+'hello'$tx}
+echo $tx
+  hello222
+  222
+
+2--
+tx=''
+echo ${tx:+'hello'$tx}
+echo $tx
+  ''
+  ''
+'
